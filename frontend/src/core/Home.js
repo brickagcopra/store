11e5858e8e7776aa9b8b2134b35dbc657bef9823
Brick Agcopra <bma5871@gmail.com>
2019-06-26 00:29:17 -0400

    first commit

---
+++ frontend/src/core/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './Layout';
+
+const Home = () => (
+    <Layout title="Home Page" description="Food Tripp For Food Lovers">
+        ...
+    </Layout>
+)
+
+
+export default Home;
